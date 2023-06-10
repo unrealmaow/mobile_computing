2dcdde20ef2d86bd8cf1c3dc29e022dc3c7898ee
Sameel Butt <officialsameel@gmail.com>
2023-06-10 10:53:10 +0500

    more code

--- lecture6/app.js
+++ lecture6/app.js
@@ -15,3 +15,46 @@ console.log(g);
 console.log(h);
 console.groupEnd();
 
+const userObject = {
+    "name": "sameel",
+    "age": 22,
+    "gender": "male",
+    "address": {
+        "street": "House # 12 Block A",
+        "city": "Gujranwala",
+        "country": "Pakistan",
+        "location": {
+            longitude: 55.5,
+            latitude: 74.3
+        }
+    }
+}
+
+console.group("Object Destructuring...");
+const {name, age, gender, address: {city}} = userObject;
+console.log(name, age, gender, city);
+console.groupEnd();
+
+console.group("Object Destructuring with rename keys");
+const {name: fullName, address: {location:{longitude: renamedLocation}}} = userObject;
+console.log(name, renamedLocation);
+console.groupEnd;
+
+//rest operator
+
+function sum(...number){
+    let sum = 0;
+    let size = number.length;
+
+    for(let i = 0; i < size; i++){
+        sum = sum + number[i];
+    }
+    return sum;
+
+}
+
+console.group("Rest Operator...");
+console.log(sum(5,7,3,2,7,1,7));
+const {name: newName, ...remainingObj} = userObject;
+console.log(newName, remainingObj);
+console.groupEnd();
